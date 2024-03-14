86922a885c5a4d18fd216ba0036a6371378b68fe
Pike1868 <luis_14mendoza@protonmail.com>
2024-03-14 16:05:15 -0400

    reverting helper file

--- backend/helpers/cryptoHelper.js
+++ backend/helpers/cryptoHelper.js
@@ -1,24 +1,20 @@
+// cryptoHelper.js
 const crypto = require("crypto");
 
-// Ensure the SECRET_KEY is of the right length
-const SECRET_KEY = crypto
-  .createHash("sha256")
-  .update(String(process.env.SECRET_KEY))
-  .digest("base64")
-  .substring(0, 32);
-const ALGORITHM = "aes-256-ctr";
-const IV = crypto.randomBytes(16);
+//Grab from env file
+const SECRET_KEY = process.env.SECRET_KEY;
+const ALGORITHM = process.env.ALGORITHM;
 
 const encrypt = (text) => {
-  const cipher = crypto.createCipheriv(ALGORITHM, SECRET_KEY, IV);
+  const cipher = crypto.createCipher(ALGORITHM, SECRET_KEY);
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return encrypted;
 };
 
-const decrypt = (encryptedText) => {
-  const decipher = crypto.createDecipheriv(ALGORITHM, SECRET_KEY, IV);
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
+const decrypt = (text) => {
+  const decipher = crypto.createDecipher(ALGORITHM, SECRET_KEY);
+  let decrypted = decipher.update(text, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
 };
